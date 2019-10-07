d02cb5f673a7f2e580eb10f402698d5ee3df5fb5
briceyokoyama <briceyokoyama@gmail.com>
2019-10-07 15:05:47 -0700

    set up firebase

---
+++ src/components/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+    <nav class="navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Meeting Log</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/lomeetingsgin">meetings</router-link>
+          <router-link class="nav-item nav-link" to="/login">login</router-link>
+          <router-link class="nav-item nav-link" to="/register">register</router-link>
+        </div>
+      </div>
+    </nav>
+
+  </div>
+</template>
